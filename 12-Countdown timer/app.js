5fcefee9ef5a64aac4414f0aae64382fc5cb6af9
Matt Stanley <Mattstanley2389@gmail.com>
2024-06-24 22:14:00 -0700

    add project

--- 12-Countdown timer/app.js
+++ 12-Countdown timer/app.js
@@ -1,54 +1,97 @@
 const months = [
   "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
+  
+  const giveaway = document.querySelector(".giveaway");
+  const deadline = document.querySelector(".deadline");
+  const items = document.querySelectorAll(".deadline-format h4");
+  
+  /* ========== setting up current and future Dates ========== */
+  
+  let currDate = new Date();
 
+  const futureDate = new Date(currDate);
+  futureDate.setDate(currDate.getDate() + 10);
+  futureDate.setHours(11, 30, 0, 0);
+  
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+  
+  let month = futureDate.getMonth();
+  month = months[month];
+  const weekday = weekdays[futureDate.getDay()];
+  const date = futureDate.getDate();
 
-// haven't implemented yet
-const giveaway = document.querySelector(".giveaway");
-const deadline = document.querySelector(".deadline");
-const items = document.querySelector(".deadline-format h4");
+  /* ============================================================ */
 
-// current time
-let currDay = Date.getDay();
-let currMonth = Date.getMonth();
-let currYear = Date.getFullYear();
+  // adjust date in hmtl
+  giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year}, ${hours}:${minutes}am`;
+  
 
-// giveaway date in object
-const futureDate = new Date(currYear, currMonth, currDay + 10, 11, 30,0,0);
+  const futureTime = futureDate.getTime();
+  
+  function getRemainingTime(){
+    const today = new Date().getTime();
+    const t = futureTime - today;
 
-// giveaway date
-const year = futureDate.getFullYear();
-const month = futureDate.getMonth();
-const day = futureDate.getDay();
-const hour = futureDate.getHour();
-const minute = futureDate.getMinutes();
-const seconds = futureDate.getSeconds();
+    // values in milliseconds
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
 
+    // calculations
+    let days = t / oneDay;
+    days = Math.floor(days);
 
-// haven't implemented yet
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
 
+    // set values in array
 
+    const values = [days, hours, minutes, seconds];
+    function format(item){
+      if (item < 10){
+        return (item = `0${item}`)
+      }
+      return item;
+    }
 
+    items.forEach((item, index) => item.innerHTML = format(values[index]));
+
+    if (t < 0){
+      clearInterval(countdown);
+      deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    }
+  };
+
+  // countdown
+  let countdown = setInterval(getRemainingTime, 1000);
+
+  // set initial values
+  getRemainingTime();
 
 
 
